430300b4732de3180d1c9a67b8df87709073fb1f
Student <Student@COMP07>
2017-11-29 10:54:04 +0200

    Add migrations

---
+++ db/migrate/20171128180954_cryptor.rb
@@ -0,0 +1,5 @@
+class Cryptor < ActiveRecord::Migration[5.1]
+  def change
+    enable_extension 'pgcrypto'
+  end
+end
---
+++ db/migrate/20171129072443_change_tasks.rb
@@ -0,0 +1,5 @@
+class ChangeTasks < ActiveRecord::Migration[5.1]
+  def change
+    #add_column :tasks, :expiry, datetime, default: Time.current
+  end
+end
