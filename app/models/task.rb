77c3e45db5b240b8e274feca86016066793b2e54
mrbertman <mrbertman.k@gmail.com>
2017-12-28 15:50:42 +0200

    One more revert

--- app/models/task.rb
+++ app/models/task.rb
@@ -1,20 +1,9 @@
 class Task < ApplicationRecord
-  include Elasticsearch::Model
-  include Elasticsearch::Model::Callbacks
-  #include Searchable
   belongs_to :user
   enum :importance => [:high, :middle, :low]
   validates :name, presence: true #{ message: I18n.t('empty_task_name') }
   validates :name, length:  { maximum: 50 }
   validates :description, length: { maximum: 500, message: "%{count} "+ I18n.t('characters_lenght_allowed') }
   validates :importance, inclusion: { :in => %w(high middle low),  message: "%{value} " + I18n.t('invalid_importance') }
-
-  mappings dynamic: false do
-    indexes :id
-    indexes :name, type: :text
-    indexes :description
-    indexes :user_id
-  end
 end
-#Task.create_index
-Task.import force: true if Rails.env.production? || Rails.env.development?
+
